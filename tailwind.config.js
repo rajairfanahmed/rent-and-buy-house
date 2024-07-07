f11d31b8adaca07a2d566f5adb9aaf55043e686b
Raja Irfan Ahmed <rajairfana551@gmail.com>
2024-07-07 17:29:25 +0500

    update: webpack, add: iconify, add: section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,34 @@
+const { addDynamicIconSelectors } = require('@iconify/tailwind');
 module.exports = {
-    content: ['./src/**/*.{html,js}'],
-    theme: {
-      extend: {
-        screens: {
-          'xxsm': '320px',
-          'xsm': '475px',
-          'sm': '640px',
-          'md': '768px',
-          'lg': '1024px',
-          'xl': '1280px',
-          '2xl': '1536px',
-        },
-        fontSize: {
-          'xs': '.75rem',
-          'sm': '.875rem',
-          'tiny': '.875rem',
-          'base': '1rem',
-          'lg': '1.125rem',
-          'xl': '1.25rem',
-          '2xl': '1.5rem',
-          '3xl': '1.875rem',
-          '4xl': '2.25rem',
-          '5xl': '3rem',
-          '6xl': '4rem',
-          '7xl': '5rem',
-        },
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {
+      screens: {
+        'xxsm': '320px',
+        'xsm': '475px',
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+      },
+      fontSize: {
+        'xs': '.75rem',
+        'sm': '.875rem',
+        'tiny': '.875rem',
+        'base': '1rem',
+        'lg': '1.125rem',
+        'xl': '1.25rem',
+        '2xl': '1.5rem',
+        '3xl': '1.875rem',
+        '4xl': '2.25rem',
+        '5xl': '3rem',
+        '6xl': '4rem',
+        '7xl': '5rem',
       },
     },
-  plugins: [],
-  };
+  },
+  plugins: [
+    addDynamicIconSelectors(),
+  ],
+};
--- webpack.common.js
+++ webpack.common.js
@@ -6,6 +6,7 @@ const TerserPlugin = require('terser-webpack-plugin');
 const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { GenerateSW } = require('workbox-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -36,6 +37,30 @@ module.exports = {
       {
         test: /\.(png|jpg|jpeg|webp|gif|svg)$/i,
         type: 'asset/resource',
+        use: [
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 75,
+              },
+              optipng: {
+                enabled: true,
+              },
+              pngquant: {
+                quality: [0.65, 0.90],
+                speed: 4,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              webp: {
+                quality: 75,
+              },
+            },
+          },
+        ],
       },
       {
         test: /\.(woff(2)?|eot|ttf|otf|svg)$/,
@@ -43,6 +68,19 @@ module.exports = {
         generator: {
           filename: 'fonts/[name][ext][query]',
         },
+      },
+      {
+        test: /\.(jpe?g|png)$/,
+        use: [
+          {
+            loader: 'webpack-image-resize-loader',
+            options: {
+              width: 800,
+              height: 600,
+              format: 'jpeg',
+            },
+          },
+        ],
       },
     ],
   },
@@ -76,6 +114,8 @@ module.exports = {
               ['jpegtran', { progressive: true }],
               ['optipng', { optimizationLevel: 5 }],
               ['svgo', { name: 'preset-default' }],
+              ['imagemin-mozjpeg', { quality: 75 }],
+              ['imagemin-webp', { quality: 75 }],
             ],
           },
         },
